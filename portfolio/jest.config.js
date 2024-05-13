b8abab4a278cb3b98df0e41b01e409c2c55815d3
Luke Holland <lukeholland15500@gmail.com>
2024-05-13 20:40:06 +0100

    Added new vite base boilerplate with tests

---
+++ portfolio/jest.config.js
@@ -0,0 +1,7 @@
+export default {
+    testEnvironment: "jsdom",
+    transform: {
+      "^.+\\.tsx?$": ['ts-jest', { diagnostics: { ignoreCodes: ['TS151001'] } }],
+    },
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  };
