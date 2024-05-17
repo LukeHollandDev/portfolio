1f55aba6827278c905737313498fee6f4fb256e1
Luke Holland <lukeholland15500@gmail.com>
2024-05-17 16:16:49 +0100

    Updated VITE_LAST_UPDATED to include offset

--- portfolio/jest.setup.ts
+++ portfolio/jest.setup.ts
@@ -1,7 +1,7 @@
 import "@testing-library/jest-dom";
 
 // Mock the date which is imported from environment variable
-const mockDate = "1970-01-01 00:00:00";
+const mockDate = "1970-01-01 00:00:00 +0100";
 jest.mock("./src/util/util", () => ({
     getEnvValue: jest.fn((key) => {
         if (key === "VITE_LAST_UPDATED") {
