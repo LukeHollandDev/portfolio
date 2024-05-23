f4d7d5abd82978cab2322cedc3403791d992bee9
Luke Holland <lukeholland15500@gmail.com>
2024-05-23 11:24:42 +0100

    Updated the jest setup util.getRelativeTime mock to include timestamp param

--- portfolio/jest.setup.ts
+++ portfolio/jest.setup.ts
@@ -3,11 +3,11 @@ import "@testing-library/jest-dom";
 // Mock the date which is imported from environment variable
 const mockDate = "1970-01-01 00:00:00 +0100";
 jest.mock("./src/util/util", () => ({
-    getEnvValue: jest.fn((key) => {
+    getEnvValue: jest.fn((key: string) => {
         if (key === "VITE_LAST_UPDATED") {
             return mockDate;
         }
         return null;
     }),
-    getRelativeTime: jest.fn(() => "1 day ago.")
+    getRelativeTime: jest.fn((timestamp: string) => `${timestamp} time ago.`)
 }));
